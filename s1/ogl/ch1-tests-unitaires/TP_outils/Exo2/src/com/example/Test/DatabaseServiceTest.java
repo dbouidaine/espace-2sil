928c03377924a27ab24b0da14869e8bd37f8fdc2
unknown <hm_boucherir@esi.dz>
2020-12-18 22:25:31 +0100

    first commit

--- s1/ogl/ch1-tests-unitaires/TP_outils/Exo2/src/com/example/Test/DatabaseServiceTest.java
+++ s1/ogl/ch1-tests-unitaires/TP_outils/Exo2/src/com/example/Test/DatabaseServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.test;
+package com.example.Test;
 
 import com.example.model.City;
 import com.example.service.DatabaseConnection;
@@ -9,7 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.sql.Connection;
 
-public class CityTest {
+public class DatabaseServiceTest{
     static DatabaseConnection connection =
             new DatabaseConnection("sa","","org.h2.Driver","jdbc:h2:mem:test");
     static Connection connect;
@@ -23,7 +23,7 @@ public class CityTest {
     @Test
     public void setGetCity(){
         City new_city = new City(1,"Alger",1,"A new city");
-        DatabaseService.addCity(CityTest.connect,new_city);
+        DatabaseService.addCity(DatabaseServiceTest.connect,new_city);
         City db_city = DatabaseService.getCity(connect,1);
         assertTrue(new_city.equals(db_city));
     }
