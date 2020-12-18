77701dfbaebd2659f26fb39418b379c169cce650
dbouidaine <hd_bouidaine@esi.dz>
2020-12-18 22:43:01 +0100

    Test Exo2 fix

--- s1/ogl/ch1-tests-unitaires/TP_outils/Exo2/src/com/example/Test/DatabaseServiceTest.java
+++ s1/ogl/ch1-tests-unitaires/TP_outils/Exo2/src/com/example/Test/DatabaseServiceTest.java
@@ -9,27 +9,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.sql.Connection;
 
-public class DatabaseServiceTest{
+public class DatabaseServiceTest {
     static DatabaseConnection connection =
-            new DatabaseConnection("sa","","org.h2.Driver","jdbc:h2:mem:test");
+            new DatabaseConnection("sa", "", "org.h2.Driver", "jdbc:h2:mem:test");
     static Connection connect;
+    static City city = new City(1, "Alger", 1, "A new city");
 
     @BeforeClass
-    public static void DatabaseConnection(){
+    public static void DatabaseConnection() {
         connect = connection.connect();
         connection.createDb(connect);
     }
 
     @Test
-    public void setGetCity(){
-        City new_city = new City(1,"Alger",1,"A new city");
-        DatabaseService.addCity(DatabaseServiceTest.connect,new_city);
-        City db_city = DatabaseService.getCity(connect,1);
-        assertTrue(new_city.equals(db_city));
+    public void addCity() {
+        int i = DatabaseService.addCity(DatabaseServiceTest.connect, city);
+        assertEquals(1, i);
+    }
+
+    @Test
+    public void getCity() {
+        City db_city = DatabaseService.getCity(connect, 1);
+        assertTrue(city.equals(db_city));
     }
 
     @AfterClass
-    public static void DatabaseDisconnection(){
+    public static void DatabaseDisconnection() {
         connection.disconnect(connect);
     }
 }
